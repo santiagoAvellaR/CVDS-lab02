bb29d9e1da4a3aec6354ac85aeb5ebb6857b1b15
unknown <santiago.avellaneda@labinfo.is.escuelaing.edu.co>
2024-08-24 13:05:52 -0500

    creando ultimas clases

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -0,0 +1,7 @@
+package Patterns.src.main.java.edu.eci.cvds.patterns.shapes;
+
+public class ShapeFactory {
+    public static final Shape create(RegularShapeType type){
+        return null;
+    }
+}
