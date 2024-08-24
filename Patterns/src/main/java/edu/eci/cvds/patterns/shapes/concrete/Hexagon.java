81ec706bebc6f86ac02cbbff8307e2faf96698b4
unknown <daniel.aldana-b@mail.escuelaing.edu.co>
2024-08-24 12:57:15 -0500

    Implementando interfaz

--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Hexagon.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Hexagon.java
@@ -1,4 +1,9 @@
 package Patterns.src.main.java.edu.eci.cvds.patterns.shapes.concrete;
+import Patterns.src.main.java.edu.eci.cvds.patterns.shapes.Shape;
 
-public class Hexagon {
+public class Hexagon implements Shape {
+    public int getNumberOfEdges(){
+        return 6;
+    }
+
 }
--- Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Triangle.java
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/concrete/Triangle.java
@@ -1,4 +1,7 @@
 package Patterns.src.main.java.edu.eci.cvds.patterns.shapes.concrete;
-
+import Patterns.src.main.java.edu.eci.cvds.patterns.shapes.Shape;
 public class Triangle {
+    public int getNumberOfEdges(){
+        return 3;
+    }
 }
